7222b09490328db7dd7ef4d80aaf19c5f1457b9d
Bak0n <Bak0n@users.noreply.github.com>
2018-02-21 20:36:12 -0500

    New appo: Just Write
    Hyperminamalist Text Editor. Just Write.

---
+++ html/justwrite/index.js
@@ -0,0 +1,22 @@
+var help = "hyperminamalist text editor. \ndark/light theme is ctrl q. \nsave is ctrl a. \nhave fun.";
+var theme = false;
+var pre = document.querySelector("pre");
+
+if (document.cookie.split("justwrite").length == 2)
+	var prev = document.cookie.split("justwrite:")[1].split(";")[0];
+pre.textContent = prev;
+
+document.addEventListener("keyup", e => {
+	if (e.ctrlKey) {
+		if (e.key == "q")
+			if (theme) {
+				document.body.style.background = "#efefef";
+				document.body.style.color = "#212121";
+			} else {
+				document.body.style.background = "#212121";
+				document.body.style.color = "#efefef";
+			}
+		if(e.key == 'a') document.cookie = "justwrite:" + pre.textContent + ";";
+		theme = !theme;
+	}
+});
