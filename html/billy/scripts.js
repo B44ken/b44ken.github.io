bf5a43783dd32d07b5a82f4f84c709a27b83915b
3ggs <calvin.barisic@mytools2go.ca>
2018-07-02 21:31:53 -0400

    Update scripts.js

--- html/billy/scripts.js
+++ html/billy/scripts.js
@@ -1,15 +1,55 @@
+var words = [
+	"kill",
+	"billy",
+	"please",
+	"ponies",
+	"gay",
+	"and",
+	"the",
+	"other",
+	"hug",
+	"lonely",
+	"depress",
+	"sauce",
+	"no",
+	"ketchup",
+	"thick",
+	"like",
+	"nick",
+	"meme",
+	"big",
+	"small",
+	"boy",
+	"girl",
+	"ooof",
+	"do",
+	"you",
+	"know",
+	"way"
+];
+var countyness = 0;
+var punctuate = [".", ".", ".", ",", ",", "!", "?", "?"];
 function theme() {
-	setTimeout(oofroast, 400);
-}
-function oofroast() {
-	alert("I don't believe in light themes");
-}
-function goodtheme() {
-	setTimeout(whatever, 400);
-}
-function whatever() {
-	alert("You are already using dark theme");
+	alert("Too hecking bad hater!")
 }
 function Function() {
-	document.getElementById("clickbait").textContent="Wait what?!?! I honestly thought that did nothing!";
+	document.getElementById("clickbait").textContent = "Kill yourself";
+}
+function random() {
+	countyness++;
+	if (countyness % 6 == 0) {
+		var randomnessinged = Math.floor(Math.random() * punctuate.length);
+		document.getElementById("randomnesses").textContent =
+			document.getElementById("randomnesses").textContent +
+			punctuate[randomnessinged];
+	} else {
+		var randomnessinged = Math.floor(Math.random() * words.length);
+		document.getElementById("randomnesses").textContent =
+			document.getElementById("randomnesses").textContent +
+			" " +
+			words[randomnessinged];
+	}
+}
+function emptify() {
+	document.getElementById("randomnesses").textContent = "";
 }
